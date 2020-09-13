ca0ed3ee33fa2b32951a0ff58989c41df0d6d37e
Filozofolcam <korayuymaz@gmail.com>
2020-09-13 14:13:04 +0300

    React and Flask connected, first build is ready

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -4,6 +4,6 @@ app = flask.Flask("__main__")
 
 @app.route("/")
 def my_index():
-    return flask.render_template("index.html", token="Hello")
+    return flask.render_template("index.html", token="Flask Token")
 
 app.run(debug=True)
