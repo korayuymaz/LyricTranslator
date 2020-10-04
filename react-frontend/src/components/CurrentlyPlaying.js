4ab96a1ca335cbe9b17769c041eecf4f25ca601c
Filozofolcam <korayuymaz@gmail.com>
2020-10-04 12:06:16 +0300

    Currently playing lyrics fixed and added css

--- react-frontend/src/components/CurrentlyPlaying.js
+++ react-frontend/src/components/CurrentlyPlaying.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/currently-playing.css';
+
 class CurrentlyPlaying extends React.Component {
     constructor(props) {
         super(props);
@@ -7,17 +8,44 @@ class CurrentlyPlaying extends React.Component {
             artist: window.token.artist,
             album_image: window.token.album_image,
             song_name: window.token.song_name,
+            lyrics: window.token.lyrics
         };
     }
+
+    // renderLyric() {
+    //     const lyrics = this.state.lyrics.replaceAll("&#34;", "").replaceAll("amp;", "").replaceAll("&#39;", "'");
+    //     let sentence = '';
+    //     let html = '';
+    //     let i;
+    //     for (i = 0; i < lyrics.length; i++) {
+    //         if (lyrics[i] === lyrics[i].toUpperCase()) {
+    //             if (sentence !== '') {
+    //                 html += <br> sentence </br>
+    //
+    //             }
+    //             sentence = lyrics[i]
+    //         } else {
+    //             sentence += lyrics[i]
+    //         }
+    //     }
+    //     return(
+    //         html
+    //     )
+    // }
+
     render() {
         const songName = this.state.song_name.toString().replaceAll("amp;", "").replaceAll("&#39;", "");
         const coverUrl = this.state.album_image.toString().replaceAll("amp;", "");
         const artist = this.state.artist.toString().replaceAll("amp;", "").replaceAll("&#39;", "'");
-        return(
+        const lyrics = this.state.lyrics.replaceAll("&#34;", "").replaceAll("amp;", "").replaceAll("&#39;", "'");
+        return (
             <div class="currently-playing">
                 <img src={coverUrl} alt={songName}/>
                 <h1>{songName}</h1>
                 <h2>{artist}</h2>
+                <p>
+                    {lyrics}
+                </p>
             </div>
         )
     }
